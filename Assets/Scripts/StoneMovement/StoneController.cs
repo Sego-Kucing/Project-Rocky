2e79c91af6dbe3c7ce82a7e57703fed1364c9c9a
Alif Rizki <alifrizki1011@gmail.com>
2026-06-26 12:51:29 +0700

    Update: Add New Line Code

--- Assets/Scripts/StoneMovement/StoneController.cs
+++ Assets/Scripts/StoneMovement/StoneController.cs
@@ -15,4 +15,23 @@ public class StoneController : MonoBehaviour
     public float maxAimAngle = 45f;
     [Tooltip("Degress per second to rotate the aim")]
     public float aimRotateSpeed = 90f;
+
+    [Header("Power Settings")]
+    [Tooltip("Max power to throw the stone")]
+    public float powerOscillateSpeed = 1.5f;
+    public float minThrowSpeed = 8f;
+    public float maxThrowSpeed = 22f;
+    [Tooltip("Upward force to apply to the stone when thrown")]
+    public float launchElevationAngle = 15f;
+
+    [Header("Flight Settings")]
+    public float gravity = 18f;
+    [Tooltip("Accelaration during hit w/s")]
+    [Range(0f, 1f)] public float verticalInputForce = 25f;
+    public float maxVerticalSpeed = 12f;
+    public float horizontalMoveSpeed = 6f;
+
+    [Header("Water & Bounce Settings")]
+    [Tooltip("World Y Height of the water surface")]
+    public float waterLevel = 0f;
 }
