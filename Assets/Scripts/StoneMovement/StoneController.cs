1f1847db635f5926f0fb3fc85134ff82788b1bd0
Alif Rizki <alifrizki1011@gmail.com>
2026-06-26 17:46:57 +0700

    Update: Add HandleAiming, HandleCharging + Launch

--- Assets/Scripts/StoneMovement/StoneController.cs
+++ Assets/Scripts/StoneMovement/StoneController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -34,4 +36,65 @@ public class StoneController : MonoBehaviour
     [Header("Water & Bounce Settings")]
     [Tooltip("World Y Height of the water surface")]
     public float waterLevel = 0f;
+    [Tooltip("If the bounce is too low, the stone will sink")]
+    public float minBounceVelocity =3f;
+    [Tooltip("If bounce need before the trampoline boost should trigger")]
+    public int bouncesBeforeBoost = 4;
+
+    [Header("Events")]
+    public UnityEvent onLaunch;
+    public BounceEvent onBounce;
+    public UnityEvent onReadyForBoost;
+    public UnityEvent onSunk;
+
+    public State CurrentState { get; private set; } = State.Aiming;
+    public float CurretPower01 { get; private set; }
+    public int BounceCount { get; private set; }
+
+    private float _aimAngle;
+    private Vector3 _velocity;
+
+    private void Update()
+    {
+        switch(CurrentState)
+        {
+            case State.Aiming: HandleAiming(); break;
+            case State.Charging: HandleCharging(); break;
+            case State.Flying: HandleFlying(); break;
+        }
+    }
+
+    private void HandleAiming()
+    {
+        float turn = 0f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) turn -= -1f;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) turn += 1f;
+
+        _aimAngle = Mathf.Clamp(_aimAngle + turn * aimRotateSpeed * Time.deltaTime, -maxAimAngle, maxAimAngle);
+         
+        if (aimPivot != null)
+            aimPivot.localRotation = Quaternion.Euler(0f, _aimAngle, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            CurrentState = State.Charging;    
+    }
+
+    private void HandleCharging()
+    {
+        CurrentPower01 = Mathf.PingPong(Time.time * powerOscillateSpeed, 1f);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Launch();
+    }
+
+    private void Launch()
+    {
+        float throwSpeed = Mathf.Lerp(minThrowSpeed, maxThrowSpeed, CurrentPower01);
+        Quaternion direction = Quaternion.Euler(-launchElevationAngle, _aimAngle, 0f);
+
+        _velocity = direction * Vector3.forward * throwSpeed;
+        BounceCount = 0;
+        CurrentState = State.Flying;
+        onLaunch?.Invoke();
+        Debug.Log($"Batunya terbang - speed {throwSpeed:F1}, aim angle: {_aimAngle:F1}");
+    }   
 }
